49d832e9de64e96dd3240dea7109c93d0eb92d5c
Eugene Karmazin <eugene@sweet.io>
2020-06-07 13:34:53 -0400

    add s3 parser function

--- lambda/s3parser/main/main.go
+++ lambda/s3parser/main/main.go
@@ -4,10 +4,16 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	//"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	//"log"
+
+	//"github.com/aws/aws-sdk-go-v2/aws/awserr"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	//"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -24,10 +30,24 @@ func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 	var dataSlice []M
 
-	m1 := M{"dn": "abc", "status": "live"}
-	m2 := M{"version": 2, "xyz": 3}
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic("failed to load config, " + err.Error())
+	}
 
-	dataSlice = append(dataSlice, m1, m2)
+	svc := s3.New(cfg)
+	input := &s3.ListObjectsInput{
+		Bucket:  aws.String("qs-production-angular-dev"),
+		MaxKeys: aws.Int64(10),
+	}
+
+	req := svc.ListObjectsRequest(input)
+	result, err := req.Send(context.Background())
+	res := result.Contents
+
+	for i := range result.Contents {
+		dataSlice = append(dataSlice, M{"Name": res[i].Key, "Time": res[i].LastModified, "URL": "https://assets.karmazin.me/" + *res[i].Key})
+	}
 
 	body, err := json.Marshal(dataSlice)
 	if err != nil {
