8cb5fe8d72cd68efd99127448f891ca8961d341d
Eugene Karmazin <eugene@sweet.io>
2020-06-07 12:16:59 -0400

    update response body output type

--- lambda/s3parser/go.mod
+++ lambda/s3parser/go.mod
@@ -2,4 +2,7 @@ module github.com/lambda/s3parser
 
 go 1.14
 
-require github.com/aws/aws-lambda-go v1.6.0
+require (
+	github.com/aws/aws-lambda-go v1.6.0
+	github.com/aws/aws-sdk-go-v2 v0.23.0 // indirect
+)
--- lambda/s3parser/main/main.go
+++ lambda/s3parser/main/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	//"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -15,16 +16,25 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
+// M as an alias for map interface
+type M map[string]interface{}
+
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
+	var dataSlice []M
 
-	body, err := json.Marshal(map[string]interface{}{
-		"Keys": "[https://assets.karmazin.me/credits.html, https://assets.karmazin.me/staging-05282020-315PM.html, https://assets.karmazin.me/staging-05282020-555PM.html]",
-	})
+	m1 := M{"dn": "abc", "status": "live"}
+	m2 := M{"version": 2, "xyz": 3}
+
+	dataSlice = append(dataSlice, m1, m2)
+
+	body, err := json.Marshal(dataSlice)
 	if err != nil {
 		return Response{StatusCode: 404}, err
 	}
+
+	//Escape fancy chars to keep JSON clean
 	json.HTMLEscape(&buf, body)
 
 	resp := Response{
