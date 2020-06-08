f69953f3fb1d6effe5c29aace3c59cc2d15479b5
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2020-06-07 20:44:30 -0400

    switch pages works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import {AuthComponent} from "./auth/auth.component";
-import {AppComponent} from "./app.component";
-import {RobotComponent} from "./robot/robot.component";
-
-const appRoutes: Routes = [
-  { path: '', component: AppComponent},
-  { path: 'robot', component: RobotComponent},
-  { path: 'auth', component: AuthComponent}
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
-})
-
-export class AppRoutingModule {
-
-}
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+//
+// import { AppComponent } from './app.component';
+// import { AuthComponent } from './auth/auth.component';
+// import { RobotComponent } from './robot/robot.component';
+//
+// const appRoutes: Routes = [
+//   { path: '', component: AppComponent}, // <-- default route to HOME page
+//   { path: 'auth', component: AuthComponent},
+//   { path: 'robot', component: RobotComponent}
+// ]
+//
+// @NgModule({
+//   imports: [RouterModule.forRoot(appRoutes)],
+//   exports: [RouterModule]
+// })
+//
+// export class AppRoutingModule {
+//
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { AuthComponent } from './auth/auth.component';
 import { RobotComponent } from './robot/robot.component';
 
 const appRoutes: Routes = [
-  // { path: '', component: AppComponent}, // <-- default route to HOME page
-  { path: 'auth', component: AuthComponent}
+  { path: '', component: AppComponent}, // <-- default route to HOME page
+  { path: 'auth', component: AuthComponent},
+  { path: 'robot', component: RobotComponent}
 ]
 
 @NgModule({
