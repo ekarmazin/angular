7e3c8c5d491129e512456cd634ce1f8f32f6a147
Eugene Karmazin <eugene@sweet.io>
2020-06-07 20:06:10 -0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from "@angular/router";
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-// import { AuthComponent } from './auth/auth.component';
+import { AuthComponent } from './auth/auth.component';
+
+const appRoutes: Routes = [
+  // { path: '', component: AppComponent}, // <-- default route to HOME page
+  { path: 'auth', component: AuthComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
-    // AuthComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpClientJsonpModule
+    HttpClientJsonpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
