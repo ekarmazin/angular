e1df1da6285febbd6726220000bf90b12010605f
Eugene Karmazin <eugene@sweet.io>
2020-06-14 22:48:27 -0400

    add manual run function structure

--- lambda/manual/main.go
+++ lambda/manual/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
@@ -8,24 +9,51 @@ import (
 	"log"
 )
 
-type myReturn struct {
-	Response string `json:"response"`
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type Response events.APIGatewayProxyResponse
+type Request events.APIGatewayProxyRequest
+
+type Data struct {
+	Email  string `json:"email"`
+	Status string `json:"status"`
 }
 
-func handle(ctx context.Context, name events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Print("Request body: ", name)
-	log.Print("context ", ctx)
-	headers := map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+func handle(ctx context.Context, req Request) (Response, error) {
+	resData := Data{}
 
 	code := 200
-	response, error := json.Marshal(myReturn{Response: "Hello, " + name.Body})
-	if error != nil {
-		log.Println(error)
-		response = []byte("Internal Server Error")
+	err := json.Unmarshal([]byte(req.Body), &resData)
+	if err != nil {
+		log.Println(err)
 		code = 500
 	}
 
-	return events.APIGatewayProxyResponse{code, headers, string(response), false}, nil
+	resData.Status = "Success"
+
+	body, err := json.Marshal(resData)
+	if err != nil {
+		log.Println(err)
+		body = []byte("Internal Server Error")
+		code = 500
+	}
+
+	var buf bytes.Buffer
+	json.HTMLEscape(&buf, body)
+
+	resp := Response{
+		StatusCode:      code,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
+			"Access-Control-Allow-Origin": "*", // <-- CORS
+		},
+	}
+	return resp, nil
+
 }
 
 func main() {
