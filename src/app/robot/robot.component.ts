91b72b7b60971a423e333372b7f22627610bfec2
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2020-06-15 22:05:33 -0400

    add post request

--- src/app/robot/robot.component.ts
+++ src/app/robot/robot.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {AuthComponent} from "../auth/auth.component";
-import {message} from "aws-sdk/clients/sns";
 
 @Component({
   selector: 'app-robot',
   templateUrl: './robot.component.html',
   styleUrls: ['./robot.component.css']
 })
+
+
+
 export class RobotComponent implements OnInit {
 
   items: any = [];
-
   userEmail= '';
 
   cron:  any = [];
@@ -29,23 +29,28 @@ export class RobotComponent implements OnInit {
   actionMethod(event: any) {
     event.target.disabled = true;
   }
+
   ngOnInit(): void {
     this.userEmail = sessionStorage.getItem('loggedUser');
-    console.log(sessionStorage.getItem('loggedUser'));
   }
 
-  onCreatePost(postData: { content: string }) {
+  onCreatePost() {
     // Send Http request
-    this.http
-      .post(
-        'https://test-5a7bf.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-
-      });
-
+    this.http.post("https://data.assets.staging.sweet.io/run/manual",
+      {
+        "email": this.userEmail ,
+      })
+      .subscribe(
+        (val) => {
+          console.log("POST call successful value returned in body",
+            val);
+        },
+        response => {
+          console.log("POST call in error", response);
+        },
+        () => {
+          console.log("The POST observable is now completed.");
+        });
   }
 }
 
